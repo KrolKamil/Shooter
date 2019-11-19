07aa32d27e347c0926b78009b02406ab36d764f6
KrolKamil <kamilkrol1997@wp.pl>
2019-11-19 20:16:44 +0100

    init

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedModerator;
+
+    private Rigidbody2D rgb2d;
+    private Vector2 currentDirrection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rgb2d = GetComponent<Rigidbody2D>();
+        this.currentDirrection = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        this.currentDirrection = new Vector2(moveHorizontal, moveVertical);
+        //transform.Translate(this.currentDirrection);
+        this.rgb2d.velocity = this.currentDirrection * speedModerator;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("XD");
+        this.currentDirrection = Vector3.zero;
+        this.rgb2d.velocity = Vector3.zero;
+    }
+}
